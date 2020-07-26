f37858195dc39f4490cfc27d41c370a9112efcdf
Ryan Stachura <stachurr@oregonstate.edu>
2020-07-26 14:10:04 -0700

    Checking to see that an empty string Fails.

--- check_pwd.py
+++ check_pwd.py
@@ -2,31 +2,8 @@
 # Ryan Stachura
 # Oregon State University
 
-def check_pwd(pwd):
-    length, lower, upper, digit, symbol = False, False, False, False, False
-    SYMBOLS = "~`!@#$%^&*()_+-="
+SYMBOLS = "~`!@#$%^&*()_+-="
 
-    # Check if password length is sufficient
-    if (len(pwd)>=8 and len(pwd)<=20):
-        length = True;
-  
-    # Check other criteria
-    for char in pwd:
-        if char.islower():
-            lower = True
-      
-        elif char.isupper():
-            upper = True
+def check_pwd(pwd):
     
-        elif char.isdigit():
-            digit = True
-      
-        elif char in SYMBOLS:
-            symbol = True
-      
-    # Check that all are true
-    if (length and lower and upper and digit and symbol):
-        return True
-    
-    else:
-        return False
+    return True
--- tests.py
+++ tests.py
@@ -8,9 +8,9 @@ from check_pwd import check_pwd
 
 
 class TestCase(unittest.TestCase):
-    
+
     def test1(self):
         self.assertFalse(check_pwd(""))
-        
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)

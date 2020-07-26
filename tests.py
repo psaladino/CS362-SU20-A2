ec0289ee2da63886253d8e1009e48f4bb0a7ff6b
Ryan Stachura <stachurr@oregonstate.edu>
2020-07-26 12:23:42 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,3 +1,10 @@
+import unittest
 from check_pwd.py import check_pwd
 
-check_pwd("")
+
+class TestCase(unittest.TestCase):
+
+    check_pwd("")
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)

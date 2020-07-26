aaba50a9c26fce528a30b470feaf2d6a492d393f
Ryan Stachura <stachurr@oregonstate.edu>
2020-07-26 16:07:21 -0700

    Either "change the bare minimum to make the test pass" is horribly worded, or TDD is horseshit. I could do the bare minimum to make it pass all day, you know where it'd get me? Absolutely nowhere. Maybe I should just strip for a living.

--- check_pwd.py
+++ check_pwd.py
@@ -6,7 +6,7 @@ SYMBOLS = "~`!@#$%^&*()_+-="
 
 def check_pwd(pwd):
 
-    if any(char.islower() for char in pwd) and any(char.isupper() for char in pwd) and any(char.isdigit() for char in pwd) and any(char in SYMBOLS for char in pwd):
-        return False
+    if any(char.islower() for char in pwd) and any(char.isupper() for char in pwd) and any(char.isdigit() for char in pwd) and any(char in SYMBOLS for char in pwd) and len(pwd)>=8 and len(pwd)<=20:
+        return True
 
     return False

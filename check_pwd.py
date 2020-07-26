a73f460a50e01b82dde76d0ae0fdfc62cf1d3c75
Ryan Stachura <stachurr@oregonstate.edu>
2020-07-26 12:26:24 -0700

    Update check_pwd.py

--- check_pwd.py
+++ check_pwd.py
@@ -1,28 +1,28 @@
 def check_pwd(pwd):
-  length, lower, upper, digit, symbol = False, False, False, False, False
-  SYMBOLS = "~`!@#$%^&*()_+-="
+    length, lower, upper, digit, symbol = False, False, False, False, False
+    SYMBOLS = "~`!@#$%^&*()_+-="
 
-  // Check if password length is sufficient
-  if (len(pwd)<8 || len(pwd)>20):
-    length = True;
+    # Check if password length is sufficient
+    if (len(pwd)>=8 and len(pwd)<=20):
+        length = True;
   
-  // Check other criteria
-  for char in pwd:
-    if char.islower():
-      lower = True
+    # Check other criteria
+    for char in pwd:
+        if char.islower():
+            lower = True
       
-    elif char.isupper():
-      upper = True
+        elif char.isupper():
+            upper = True
     
-    elif char.isdigit():
-      digit = True
+        elif char.isdigit():
+            digit = True
       
-    elif char in SYMBOLS:
-      symbol = True
+        elif char in SYMBOLS:
+            symbol = True
       
-    // Check that all are true
+    # Check that all are true
     if (length and lower and upper and digit and symbol):
-      return True
+        return True
     
     else:
-      return False;
+        return False
